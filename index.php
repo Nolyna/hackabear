e4604e3a4a1155d8b2c1f47c0f16974ad7d34d1e
Nolyna <Nolyna@users.noreply.github.com>
2018-10-06 20:47:16 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
-echo '<h1> Hello <h1> ' 
-$method = $_SERVER['REQUEST_METHOD'];
+/*$method = $_SERVER['REQUEST_METHOD'];
 
 // Process only when method is POST
 if($method == 'POST'){
@@ -22,6 +21,47 @@ if($method == 'POST'){
 else
 {
 	echo "Method not allowed";
+}*/
+
+
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Process only when method is POST
+if($method == 'POST'){
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+
+	$text = $json->result->parameters->text;
+
+	switch ($text) {
+		case 'hi':
+			$speech = "Hi, Nice to meet you";
+			break;
+
+		case 'bye':
+			$speech = "Bye, good night";
+			break;
+
+		case 'anything':
+			$speech = "Yes, you can type anything here.";
+			break;
+
+		default:
+			$speech = "Sorry, I didnt get that. Please ask me something else.";
+			break;
+	}
+
+	$response = new \stdClass();
+	$response->speech = $speech;
+	$response->displayText = $speech;
+	$response->source = "webhook";
+	echo json_encode($response);
 }
+else
+{
+	echo "Method not allowed";
+}
+
 
 ?>
